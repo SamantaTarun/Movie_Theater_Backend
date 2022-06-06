f4b2e93542bb0ed1e8c7fbc25e91c9ee89f3f19a
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-06-06 13:02:20 +0530

    style: fixed the variable naming

--- src/main/kotlin/com/demo/book/movie/service/MovieService.kt
+++ src/main/kotlin/com/demo/book/movie/service/MovieService.kt
@@ -5,13 +5,13 @@ import com.demo.book.movie.request.MovieRequest
 import javax.inject.Inject
 import javax.inject.Singleton
 
-const val MOVIE_DURATION_LOWER_LIMIT_IN_SECONDS = 5
-const val MOVIE_DURATION_UPPER_LIMIT_IN_SECONDS = 360
+const val MOVIE_DURATION_LOWER_LIMIT_IN_MINUTES = 5
+const val MOVIE_DURATION_UPPER_LIMIT_IN_MINUTES = 360
 @Singleton
 class MovieService(@Inject val movieRepository: MovieRepository) {
     fun save(movieRequest: MovieRequest): Movie {
         val durationInSeconds = movieRequest.duration
-        if (durationInSeconds !in MOVIE_DURATION_LOWER_LIMIT_IN_SECONDS..MOVIE_DURATION_UPPER_LIMIT_IN_SECONDS)
+        if (durationInSeconds !in MOVIE_DURATION_LOWER_LIMIT_IN_MINUTES..MOVIE_DURATION_UPPER_LIMIT_IN_MINUTES)
             throw InvalidDurationException("")
         return movieRepository.save(movieRequest)
     }
